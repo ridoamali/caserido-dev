fda20a9da8846f1c34bb669a56111b574d032e6b
Rido A <98310463+ridoamali@users.noreply.github.com>
2024-05-31 09:59:32 +0700

    Update actions.ts

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -64,7 +64,7 @@ export const createCheckoutSession = async ({ configId }: { configId: string }) 
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ["card", "paypal", "wechat_pay", "alipay"],
+    payment_method_types: ["card"],
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["DE", "US", "ID", "SG", "MY"] },
     metadata: {
